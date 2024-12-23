6715e597b6ee8648176fcbb5845147bd5538c299
Pablo Querales <pabloquerales20@gmail.com>
2024-12-23 12:06:02 +0100

    se planteó una estrategia para avanzar con el proyecto

--- src/js/component/gridPlay.jsx
+++ src/js/component/gridPlay.jsx
@@ -7,19 +7,19 @@ const Boardgame = () => {
 			<button>Start Over</button>
 			<div className="container text-center ">
 				<div className="row">
-					<div className="col"></div>
-					<div className="col"></div>
-					<div className="col"></div>
+					<div className="col">0</div>
+					<div className="col">1</div>
+					<div className="col">2</div>
 				</div>
 				<div className="row">
-					<div className="col"></div>
-					<div className="col"></div>
-					<div className="col"></div>
+					<div className="col">3</div>
+					<div className="col">4</div>
+					<div className="col">5</div>
 				</div>
 				<div className="row">
-					<div className="col"></div>
-					<div className="col"></div>
-					<div className="col"></div>
+					<div className="col">6</div>
+					<div className="col">7</div>
+					<div className="col">8</div>
 				</div>
 			</div>
 		</div>
